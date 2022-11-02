f23191d771f63e099c2c6e209120acfa37fc7cbc
Sara Raley <sara.m.raley@gmail.com>
2022-11-02 11:26:26 -0600

    adds routes for hippo and dragon to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/tiny_hippo", controller: "products", action: "tiny_hippo"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/sleepy_dragon", controller: "products", action: "sleepy_dragon"
 end
