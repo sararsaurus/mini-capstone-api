02894dc96b4583df4ba13475ab8633119fb9a065
Sara Raley <sara.m.raley@gmail.com>
2022-11-02 14:14:01 -0600

    adds cat product

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-product = Product.create(name: "Tiny Hippo", price: 15, image_url: "https://kristitullus.com/blogs/news/free-pattern-small-hippo", description: "A tiny hippo with a huge nose and stumpy little legs, small enough to sit on your palm.")
+Product.create(name: "Tiny Hippo", price: 15, image_url: "https://kristitullus.com/blogs/news/free-pattern-small-hippo", description: "A tiny hippo with a huge nose and stumpy little legs, small enough to sit on your palm.")
 
-product = Product.create(name: "Sleepy Dragon", price: 30, image_url: "https://hobbii.com/donnie-the-dino", description: "A baby comforter in the shape of a snuggly dragon.")
+Product.create(name: "Sleepy Dragon", price: 30, image_url: "https://hobbii.com/donnie-the-dino", description: "A baby comforter in the shape of a snuggly dragon.")
+
+Product.create(name: "Bad Cat", price: 5, image_url: "https://thoresbycottage.com/2018/08/28/crochet-cat-pattern/", description: "A somewhat abstract interpretation of a cat.")
