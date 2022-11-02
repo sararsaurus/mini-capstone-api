c0781c37b7194571320c6fa0c15c15b19efa0d4c
Sara Raley <sara.m.raley@gmail.com>
2022-11-02 11:22:55 -0600

    adds hippo and dragon to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.create(name: "Tiny Hippo", price: 15, image_url: "https://kristitullus.com/blogs/news/free-pattern-small-hippo", description: "A tiny hippo with a huge nose and stumpy little legs, small enough to sit on your palm.")
+
+product = Product.create(name: "Sleepy Dragon", price: 30, image_url: "https://hobbii.com/donnie-the-dino", description: "A baby comforter in the shape of a snuggly dragon.")
