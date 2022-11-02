f746e521581cc4ca9d1cfa6912f7c90beafdcf05
Sara Raley <sara.m.raley@gmail.com>
2022-11-02 11:40:00 -0600

    adds route that displays JSON for all available products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "/tiny_hippo", controller: "products", action: "tiny_hippo"
+  # get "/tiny_hippo", controller: "products", action: "tiny_hippo"
 
-  get "/sleepy_dragon", controller: "products", action: "sleepy_dragon"
+  # get "/sleepy_dragon", controller: "products", action: "sleepy_dragon"
+
+  get "/all_products", controller: "products", action: "all_products"
 end
