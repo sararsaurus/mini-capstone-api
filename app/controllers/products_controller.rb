f68e0d2daf5d7021db346bf41a4a91f1aba090c4
Sara Raley <sara.m.raley@gmail.com>
2022-11-03 11:47:33 -0600

    adds requests.http file to test the get and post commands

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,23 +4,35 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
-  def all_products
-    products = Product.all
-    render json: products.as_json
-  end
-
-  def first_product
-    product = Product.first
-    render json: product.as_json
-  end
-
-  def second_product
-    product = Product.second
-    render json: product.as_json
-  end
-
-  def one_product
+  def show
     product = Product.find_by(id: params["id"])
     render json: product.as_json
   end
+
+  def create
+    product = Product.new(
+      name: "Baby Octopus",
+      price: 5,
+      image_url: "https://images4-f.ravelrycache.com/uploads/AllFromJade/722750265/IMG_0660_small2.JPG",
+      description: "Tiny baby octopus that can be customized into aliens (three eyes) or cyclopes (one eye)",
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  # def all_products
+  #   products = Product.all
+  #   render json: products.as_json
+  # end
+
+  # def first_product
+  #   product = Product.first
+  #   render json: product.as_json
+  # end
+
+  # def second_product
+  #   product = Product.second
+  #   render json: product.as_json
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
 
   # get "/sleepy_dragon", controller: "products", action: "sleepy_dragon"
 
-  get "/all_products", controller: "products", action: "all_products"
+  # get "/all_products", controller: "products", action: "all_products"
 
-  get "/first_product", controller: "products", action: "first_product"
+  # get "/first_product", controller: "products", action: "first_product"
 
-  get "/second_product", controller: "products", action: "second_product"
-
-  get "/one_product/:id", controller: "products", action: "one_product"
+  # get "/second_product", controller: "products", action: "second_product"
 
   get "/products" => "products#index"
+
+  get "/products/:id" => "products#show"
+
+  post "/products" => "products#create"
 end
