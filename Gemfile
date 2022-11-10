3c352c126a03bd5e5e2224ba515171420c027514
Sara Raley <sara.m.raley@gmail.com>
2022-11-10 11:42:27 -0700

    adds jwt

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem "jb"
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
@@ -46,4 +46,6 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
+
+gem 'jwt'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
-
   get "/products/:id" => "products#show"
-
   post "/products" => "products#create"
-
   patch "/products/:id" => "products#update"
-
   delete "/products/:id" => "products#destroy"
 
-  # get "/suppliers" => "suppliers#index"
+  get "/suppliers" => "suppliers#index"
+  get "/suppliers/:id" => "suppliers#show"
+  post "/suppliers" => "suppliers#create"
+  patch "/suppliers/:id" => "suppliers#update"
+  delete "/suppliers/:id" => "suppliers#destroy"
 
-  # get "/suppliers/:id" => "suppliers#show"
+  post "/users" => "users#create"
 
-  # post "/suppliers" => "suppliers#create"
-
-  # patch "/suppliers/:id" => "suppliers#update"
-
-  # delete "/suppliers/:id" => "suppliers#destroy"
+  post "/sessions" => "sessions#create"
 end
