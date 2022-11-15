46e7380fac6b9b6c6e64c5bb0fcd7aa06c31c5c5
Sara Raley <sara.m.raley@gmail.com>
2022-11-15 10:12:08 -0700

    updates

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
